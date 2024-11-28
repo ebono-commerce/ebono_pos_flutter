df87a29d3f1a1fa9ea85a77abc567073b2fb06d2
JagathRatchaganIbo <jagath.ratchagan@ibo.com>
2024-11-28 15:41:46 +0530

    window closing popup

--- lib/utils/common_methods.dart
+++ lib/utils/common_methods.dart
@@ -2,6 +2,7 @@ import 'package:ebono_pos/constants/custom_colors.dart';
 import 'package:ebono_pos/ui/Common_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_window_close/flutter_window_close.dart';
+import 'package:get/get.dart';
 
 bool isValidOfferId(String offerId) {
   return offerId.length == 8 ||
@@ -10,8 +11,8 @@ bool isValidOfferId(String offerId) {
       offerId.length == 13;
 }
 
-showCloseAlert(BuildContext context) {
-  var alertShowing = false;
+showCloseAlert(BuildContext context, {bool isAlertShowing = false}) {
+  var alertShowing = isAlertShowing;
 
   ThemeData theme = Theme.of(context);
 
@@ -20,52 +21,51 @@ showCloseAlert(BuildContext context) {
     if (alertShowing) return false;
     alertShowing = true;
 
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-              title: const Text('Do you really want to quit?'),
-              actions: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(12),
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                          color: CustomColors.primaryColor, width: 1.5),
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    backgroundColor: CustomColors.keyBoardBgColor,
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                    alertShowing = false;
-                  },
-                  child: Text(
-                    'Yes',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
-                  ),
+    return await Get.dialog(
+      AlertDialog(
+          title: const Text('Do you really want to quit?'),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(12),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      color: CustomColors.primaryColor, width: 1.5),
+                  borderRadius: BorderRadius.circular(14),
                 ),
-                ElevatedButton(
-                  style: commonElevatedButtonStyle(
-                      theme: theme,
-                      textStyle: theme.textTheme.bodyMedium,
-                      padding: EdgeInsets.all(12)),
-                  child: Text(
-                    'No',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                    alertShowing = false;
-                  },
-                ),
-              ]);
-        });
+                backgroundColor: CustomColors.keyBoardBgColor,
+              ),
+              onPressed: () {
+                alertShowing = false;
+                Get.back(result: true);
+              },
+              child: Text(
+                'Yes',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            ElevatedButton(
+              style: commonElevatedButtonStyle(
+                  theme: theme,
+                  textStyle: theme.textTheme.bodyMedium,
+                  padding: EdgeInsets.all(12)),
+              child: Text(
+                'No',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                alertShowing = false;
+                Get.back();
+              },
+            ),
+          ]),
+      barrierDismissible: true,
+    );
   });
 }
