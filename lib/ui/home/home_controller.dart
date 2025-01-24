7a28d3230fe805d3c733befd799d20e239bcd826
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-01-24 11:19:06 +0530

    reverted missing code

--- lib/ui/home/home_controller.dart
+++ lib/ui/home/home_controller.dart
@@ -338,7 +338,7 @@ class HomeController extends GetxController {
         if (cartId.value != "" && (response.priceList?.length ?? 0) <= 1) {
           addToCartApiCall(
             scanProductsResponse.value.skuCode,
-            1,
+            scanProductsResponse.value.isWeighedItem == true ? 0 : 1,
             scanProductsResponse.value.priceList!.first.mrpId,
             scanProductsResponse.value.salesUom,
             cartId.value,
