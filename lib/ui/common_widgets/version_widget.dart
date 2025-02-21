ab54afe2465fedbe2dcc896040f088f3cb7362ab
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-02-21 11:24:34 +0530

    updated prod release version

--- lib/ui/common_widgets/version_widget.dart
+++ lib/ui/common_widgets/version_widget.dart
@@ -8,7 +8,7 @@ class VersionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String environment = String.fromEnvironment('ENV', defaultValue: 'stage');
+    String environment = String.fromEnvironment('ENV', defaultValue: 'prod');
 
     return FutureBuilder<PackageInfo>(
       future: PackageInfo.fromPlatform(),
