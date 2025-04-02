1316bd4215c3fe5e403e577d74a8bd4df65aad9a
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-04-02 14:23:36 +0530

    added network check in auth interceptor with message

--- lib/api/auth_interceptor.dart
+++ lib/api/auth_interceptor.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:ebono_pos/api/api_constants.dart';
 import 'package:ebono_pos/api/environment_config.dart';
@@ -18,6 +19,12 @@ class AuthInterceptor extends Interceptor {
     String? token = await _sharedPreferenceHelper.getAuthToken();
     String? appUUID = await _sharedPreferenceHelper.getAppUUID();
     String pointedTo = await _sharedPreferenceHelper.pointingTo();
+
+    final connectivityResult = await Connectivity().checkConnectivity();
+
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.snackbar('NO INTERNET', 'please check internet connection');
+    }
 
     if (options.uri.path.contains('/api/3.0/p2p/')) {
       options.baseUrl = EnvironmentConfig.paymentBaseUrl;
