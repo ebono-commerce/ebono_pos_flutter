a3998f3da88661dd57538347488bfec06faad438
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-04-08 16:13:54 +0530

    Scan Api Now Accepts 9 Digit BarCode

--- lib/utils/common_methods.dart
+++ lib/utils/common_methods.dart
@@ -11,6 +11,7 @@ import 'package:get/get.dart';
 bool isValidOfferId(String offerId) {
   return offerId.length == 6 ||
       offerId.length == 8 ||
+      offerId.length == 9 ||
       offerId.length == 10 ||
       offerId.length == 12 ||
       offerId.length == 13;
