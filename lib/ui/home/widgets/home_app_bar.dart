454fa69e501ca369fe357adc51ac3a4236a4c0fc
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-04-09 15:28:36 +0530

    UI Enhancements

--- lib/ui/home/widgets/home_app_bar.dart
+++ lib/ui/home/widgets/home_app_bar.dart
@@ -42,89 +42,121 @@ class _HomeAppBarState extends State<HomeAppBar> {
           widget.titleWidget,
           Spacer(),
           Flexible(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Obx(
-                  () => Text(
-                    '${homeController.userDetails.value.fullName}| ${homeController.selectedOutlet.value}',
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelMedium
-                        ?.copyWith(color: CustomColors.black),
-                    // style: TextStyle(color: Colors.black, fontSize: 13),
+            child: Center(
+              child:
+                  // Column(
+                  //   children: [
+                  //     Obx(
+                  //       () => Text(
+                  //         '${homeController.userDetails.value.fullName} | ${homeController.selectedOutlet.value}',
+                  //         style: Theme.of(context)
+                  //             .textTheme
+                  //             .labelMedium
+                  //             ?.copyWith(color: CustomColors.black),
+                  //         // style: TextStyle(color: Colors.black, fontSize: 13),
+                  //       ),
+                  //     ),
+                  //     SizedBox(height: 5),
+                  //     Obx(
+                  //       () => Text(
+                  //         '${homeController.pointedTo.value} | ${homeController.selectedTerminal.value}',
+                  //         style: Theme.of(context).textTheme.labelMedium,
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                  Column(
+                children: [
+                  Obx(
+                    () => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            homeController.userDetails.value.fullName,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium
+                                ?.copyWith(color: CustomColors.black),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.end,
+                          ),
+                        ),
+                        Text(
+                          ' | ',
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        Expanded(
+                          child: Text(
+                            homeController.selectedOutlet.value,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium
+                                ?.copyWith(color: CustomColors.black),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.start,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      // onTap: () {
-                      //   setState(() {
-                      //     isOnline = !isOnline;
-                      //   });
-                      // },
-                      child: Row(
-                        children: [
-                          Obx(() => ImageIcon(
+                  SizedBox(height: 5),
+                  Obx(
+                    () => Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            spacing: 10,
+                            children: [
+                              ImageIcon(
                                 size: 20,
-                                color: homeController.isOnline.value == true
+                                color: homeController.pointedTo.value == 'CLOUD'
                                     ? CustomColors.green
                                     : CustomColors.red,
                                 homeController.isOnline.value == true
                                     ? AssetImage('assets/images/ic_online.png')
                                     : AssetImage(
                                         'assets/images/ic_offline.png'),
-                              )),
-                          SizedBox(width: 5),
-                          Obx(() => Text(
-                                homeController.isOnline.value == true
-                                    ? 'ONLINE'
-                                    : "OFFLINE",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelMedium
-                                    ?.copyWith(
-                                        color: homeController.isOnline.value ==
-                                                true
-                                            ? CustomColors.green
-                                            : CustomColors.red),
-                                //  style: TextStyle(color: Colors.green, fontSize: 11),
-                              )),
-                        ],
-                      ),
+                              ),
+                              Text(
+                                homeController.pointedTo.value,
+                                style: Theme.of(context).textTheme.labelMedium,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.end,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          ' | ',
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        Expanded(
+                          child: Text(
+                            homeController.selectedTerminal.value,
+                            style: Theme.of(context).textTheme.labelMedium,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.start,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 5),
-                    Obx(
-                      () => Text(
-                        homeController.selectedTerminal.value,
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium
-                            ?.copyWith(color: CustomColors.black),
-                        //  style: TextStyle(color: Colors.black, fontSize: 12),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
+          Spacer()
         ],
       ),
       actions: [
         Padding(
           padding: const EdgeInsets.only(right: 10.0),
-          child: LogoutButton(
-              buttonWidth: widget.showBackButton ? 140 : null ),
+          child: LogoutButton(buttonWidth: widget.showBackButton ? 140 : null),
         )
       ],
-
       toolbarHeight: kToolbarHeight,
     );
   }
