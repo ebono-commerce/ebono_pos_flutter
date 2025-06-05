421ca1ccc350e6c591810f03eef009c994a9bb99
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-06-05 10:27:21 +0530

    updated full screen code

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -8,33 +8,35 @@
 #include "flutter/generated_plugin_registrant.h"
 
 struct _MyApplication {
-  GtkApplication parent_instance;
-  char** dart_entrypoint_arguments;
+    GtkApplication parent_instance;
+    char** dart_entrypoint_arguments;
+    GtkWindow* main_window;  // Track the window
 };
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
-// Handle focus-in events to ensure full screen state is maintained
+/// Called when the window gains focus
 static gboolean on_window_focus_in(GtkWidget* widget, GdkEventFocus* event, gpointer user_data) {
-    // Re-enable fullscreen when focus returns to the application
+    // Reapply fullscreen if lost
     gtk_window_fullscreen(GTK_WINDOW(widget));
     return FALSE;
 }
 
-// Implements GApplication::activate.
+// GApplication::activate - app launched or clicked from dock
 static void my_application_activate(GApplication* application) {
-  MyApplication* self = MY_APPLICATION(application);
-  GtkWindow* window =
-      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+    MyApplication* self = MY_APPLICATION(application);
 
-  // Use a header bar when running in GNOME as this is the common style used
-  // by applications and is the setup most users will be using (e.g. Ubuntu
-  // desktop).
-  // If running on X and not using GNOME then just use a traditional title bar
-  // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
-  gboolean use_header_bar = TRUE;
+    // If window already exists, bring it to front and ensure fullscreen
+    if (self->main_window != NULL) {
+        gtk_window_present(self->main_window);
+        gtk_window_fullscreen(self->main_window);
+        return;
+    }
+
+    GtkWindow* window = GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+    self->main_window = window;
+
+    gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
     GdkScreen* screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen)) {
@@ -44,96 +46,102 @@ static void my_application_activate(GApplication* application) {
     }
   }
 #endif
-  if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ebono_pos");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  } else {
-    gtk_window_set_title(window, "ebono_pos");
-  }
 
-  gtk_window_set_default_size(window, 1280, 720);
-  gtk_window_fullscreen(GTK_WINDOW(window));
+    if (use_header_bar) {
+        GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+        gtk_widget_show(GTK_WIDGET(header_bar));
+        gtk_header_bar_set_title(header_bar, "ebono_pos");
+        gtk_header_bar_set_show_close_button(header_bar, TRUE);
+        gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+    } else {
+        gtk_window_set_title(window, "ebono_pos");
+    }
 
-  // Connect focus-in event signal to handle regaining focus
-  g_signal_connect(G_OBJECT(window), "focus-in-event", G_CALLBACK(on_window_focus_in), NULL);
+    gtk_window_set_default_size(window, 1280, 720);
 
-  // Set window type hint to stay on top
-  gtk_window_set_keep_above(window, TRUE);
+    // Initial fullscreen
+    gtk_window_fullscreen(window);
 
-  gtk_widget_show(GTK_WIDGET(window));
+    // Keep on top if desired
+    gtk_window_set_keep_above(window, TRUE);
 
-  g_autoptr(FlDartProject) project = fl_dart_project_new();
-  fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
+    // Reapply fullscreen whenever window regains focus
+    g_signal_connect(G_OBJECT(window), "focus-in-event", G_CALLBACK(on_window_focus_in), NULL);
 
-  FlView* view = fl_view_new(project);
-  gtk_widget_show(GTK_WIDGET(view));
-  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
+    gtk_widget_show(GTK_WIDGET(window));
 
-  fl_register_plugins(FL_PLUGIN_REGISTRY(view));
+    // Create the Flutter view
+    g_autoptr(FlDartProject) project = fl_dart_project_new();
+    fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
-  gtk_widget_grab_focus(GTK_WIDGET(view));
+    FlView* view = fl_view_new(project);
+    gtk_widget_show(GTK_WIDGET(view));
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
+
+    // Register plugins and grab focus
+    fl_register_plugins(FL_PLUGIN_REGISTRY(view));
+    gtk_widget_grab_focus(GTK_WIDGET(view));
 }
 
-// Implements GApplication::local_command_line.
+// GApplication::local_command_line
 static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
-  MyApplication* self = MY_APPLICATION(application);
-  // Strip out the first argument as it is the binary name.
-  self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
+    MyApplication* self = MY_APPLICATION(application);
 
-  g_autoptr(GError) error = nullptr;
-  if (!g_application_register(application, nullptr, &error)) {
-     g_warning("Failed to register: %s", error->message);
-     *exit_status = 1;
-     return TRUE;
-  }
+    // Store Dart entrypoint args
+    self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
 
-  g_application_activate(application);
-  *exit_status = 0;
+    g_autoptr(GError) error = NULL;
+    if (!g_application_register(application, NULL, &error)) {
+        g_warning("Failed to register: %s", error->message);
+        *exit_status = 1;
+        return TRUE;
+    }
 
-  return TRUE;
+    g_application_activate(application);
+    *exit_status = 0;
+    return TRUE;
 }
 
-// Implements GApplication::startup.
+// GApplication::startup
 static void my_application_startup(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application startup.
-
-  G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
+    G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
 }
 
-// Implements GApplication::shutdown.
+// GApplication::shutdown
 static void my_application_shutdown(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application shutdown.
-
-  G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
+    G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
 }
 
-// Implements GObject::dispose.
+// GObject::dispose
 static void my_application_dispose(GObject* object) {
-  MyApplication* self = MY_APPLICATION(object);
-  g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
-  G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
+    MyApplication* self = MY_APPLICATION(object);
+
+    g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
+
+    if (self->main_window != NULL) {
+        gtk_widget_destroy(GTK_WIDGET(self->main_window));
+        self->main_window = NULL;
+    }
+
+    G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
 }
 
+// Class init
 static void my_application_class_init(MyApplicationClass* klass) {
-  G_APPLICATION_CLASS(klass)->activate = my_application_activate;
-  G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
-  G_APPLICATION_CLASS(klass)->startup = my_application_startup;
-  G_APPLICATION_CLASS(klass)->shutdown = my_application_shutdown;
-  G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
+    G_APPLICATION_CLASS(klass)->activate = my_application_activate;
+    G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
+    G_APPLICATION_CLASS(klass)->startup = my_application_startup;
+    G_APPLICATION_CLASS(klass)->shutdown = my_application_shutdown;
+    G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
 }
 
+// Instance init
 static void my_application_init(MyApplication* self) {}
 
+// Factory method
 MyApplication* my_application_new() {
-  return MY_APPLICATION(g_object_new(my_application_get_type(),
-                                     "application-id", APPLICATION_ID,
-                                     "flags", G_APPLICATION_NON_UNIQUE,
-                                     nullptr));
+    return MY_APPLICATION(g_object_new(my_application_get_type(),
+                                       "application-id", APPLICATION_ID,
+                                       "flags", G_APPLICATION_NON_UNIQUE,
+                                       NULL));
 }
