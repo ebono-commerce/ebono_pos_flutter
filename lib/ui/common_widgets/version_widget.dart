49ac3508131f495baaf29456352c754ee0f2388f
ItsReddyGona <gona.janardhanreddy@gmail.com>
2025-06-27 16:49:09 +0530

    ready for prod

--- lib/ui/common_widgets/version_widget.dart
+++ lib/ui/common_widgets/version_widget.dart
@@ -11,7 +11,7 @@ class VersionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sharedPrefs = Get.find<SharedPreferenceHelper>();
-    String environment = String.fromEnvironment('ENV', defaultValue: 'stage');
+    String environment = String.fromEnvironment('ENV', defaultValue: 'prod');
 
     return FutureBuilder<Map<String, dynamic>>(
       future: Future.wait([
